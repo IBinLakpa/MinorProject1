936107081602f2a0ee78ae6c5cba3e8f2d7b7436
IBinLakpa <107236919+IBinLakpa@users.noreply.github.com>
2024-02-19 20:38:16 +0545

    Add files via upload
    bar and sidebar v2

--- mp1/bar.php
+++ mp1/bar.php
@@ -1,28 +1,29 @@
-<link rel="stylesheet" href="css/bar.css">
+<link rel="stylesheet" href="css/bar.css?">
 <header>
-        <a href="index.php?page=1#" class="home-btn">Home</a>
+    <img src="images/menu.svg" alt="" class="menu svg-image" onclick="togglemenu()">
+    <div class="barcontent">
+        <a href="index.php" class="logo"><img src="images/logo.svg"></a>
         
-    <?php
-        session_start();
-        if(isset($_SESSION['name'])){
-            echo '
-                <div class="dropdown">
-                    <button class="dropbtn">Hi '.$_SESSION['name'].' ▼</button>
-                    <div class="dropdown-content">
-                        <a href="profile.php" class="btn">My Profile</a>
-                        <a href="history.php" class="btn">History</a>
-                        <a href="logout.php" class="btn">Logout</a>
+        <?php
+            session_start();
+            if(isset($_SESSION['name'])){
+                echo '
+                    <div class="dropdown">
+                        <div class="dropbtn">Hi '.$_SESSION['name'].' ▼</div>
+                        <div class="dropdown-content">
+                            <a href="profile.php" class="btn">My Profile</a>
+                            <a href="history.php" class="btn">History</a>
+                            <a href="logout.php" class="btn">Logout</a>
+                        </div>
                     </div>
-                </div>
-            ';
-        }
-        else{
-            echo '
-                
-                    <span class= "home_btn"><a href="login_form.php">Log In</a> / <a href="register_form.php">Sign up</a></span>
-            ';
-        }
-    ?>
+                ';
+            }
+            else{
+                echo '
+                    <div><a href="login.php">Log In</a> / <a href="register.php">Sign up</a></div>
+                ';
+            }
+        ?>
+    </div>
 </header>
-<a href="cart.php" class="btn"><img class='cart'src='cart.jpg' alt="Cart"></a>
-<link rel="stylesheet" href="css/style.css">
+<img src="images/cart.svg" alt="" class="cart svg-image" onclick="togglemenu()">

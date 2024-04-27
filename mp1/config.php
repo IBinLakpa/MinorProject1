86ef6eafab1b92ecb7ff66735ab9111b0bb44193
IBinLakpa <107236919+IBinLakpa@users.noreply.github.com>
2024-04-27 09:58:43 +0545

    version 3

--- mp1/config.php
+++ mp1/config.php
@@ -3,8 +3,10 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    // Check if the connection was successful
-    if (!$conn) {
-        die('Connection failed: ' . mysqli_connect_error());
+    function get_conn(){
+        $conn = mysqli_connect('localhost','root','','online_storefront');
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
     }
-?>
